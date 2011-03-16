aa392aff4d1bccaaedb730307342d550b8f9ed37
s.mardine <s.mardine@gmail.com>
2011-03-16 13:23:30 +0000

    activation du filtrage par un seul bouton sur la vue recherche seulement pour l'instant

---  ma-ptite-trousse --username s.mardine@gmail.com/matroussedemaquillage/src/fr/smardine/matroussedemaquillage/variableglobale/EnCategorieFiltrage.java
+++  ma-ptite-trousse --username s.mardine@gmail.com/matroussedemaquillage/src/fr/smardine/matroussedemaquillage/variableglobale/EnCategorieFiltrage.java
@@ -1,36 +1,58 @@
 package fr.smardine.matroussedemaquillage.variableglobale;
 
-/**
- * @author smardine
- */
-public enum EnCategorieAutres {
-	Pinceaux(0, "Pinceaux"), //
-	VernisAongles(1, "Vernis à ongles");
+public enum EnCategorieFiltrage {
+	/**
+	 * 
+	 */
+	Tout(0, "Tout"), //
+	/**
+	 * 
+	 */
+	Marque(1, "Marques"), //
+	/**
+	 * 
+	 */
+	Categorie(2, "Catégories");
 
 	private int code;
 	private String lib;
 
-	private EnCategorieAutres(int p_code, String p_lib) {
+	private EnCategorieFiltrage(int p_code, String p_lib) {
 		code = p_code;
 		lib = p_lib;
 	}
 
+	/**
+	 * @return
+	 */
 	public Long getIdent() {
 		return Long.valueOf(ordinal());
 	}
 
+	/**
+	 * @return
+	 */
 	public int getCode() {
 		return code;
 	}
 
+	/**
+	 * @param p_code
+	 */
 	public void setCode(int p_code) {
 		code = p_code;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getLib() {
 		return lib;
 	}
 
+	/**
+	 * @param p_lib
+	 */
 	public void setLib(String p_lib) {
 		lib = p_lib;
 	}
