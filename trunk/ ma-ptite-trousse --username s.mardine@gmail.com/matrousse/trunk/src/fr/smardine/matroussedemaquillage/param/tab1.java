3291719821a4bd3559324b61c374dbb7031cb333
s.mardine@gmail.com <s.mardine@gmail.com@d13b75dc-5ad4-d725-d88b-725a437c4c4c>
2012-04-24 17:13:38 +0000

    creation d'une super activity pour les parametres

--- trunk/ ma-ptite-trousse --username s.mardine@gmail.com/matrousse/trunk/src/fr/smardine/matroussedemaquillage/param/tab1.java
+++ trunk/ ma-ptite-trousse --username s.mardine@gmail.com/matrousse/trunk/src/fr/smardine/matroussedemaquillage/param/tab1.java
@@ -1,13 +1,10 @@
 package fr.smardine.matroussedemaquillage.param;
 
 import widget.majWidget;
-import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -19,26 +16,16 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import fr.smardine.matroussedemaquillage.Main;
 import fr.smardine.matroussedemaquillage.R;
 import fr.smardine.matroussedemaquillage.base.BDAcces;
 import fr.smardine.matroussedemaquillage.base.accesTable.AccesTableParams;
-import fr.smardine.matroussedemaquillage.note.note_page1;
-import fr.smardine.matroussedemaquillage.note.note_saisie;
-import fr.smardine.matroussedemaquillage.recherche.Recherche;
-import fr.smardine.matroussedemaquillage.recherche.affiche_detail;
-import fr.smardine.matroussedemaquillage.recherche.recherche_produit_perime;
-import fr.smardine.matroussedemaquillage.remplir.choix_produit_a_duppliquer;
-import fr.smardine.matroussedemaquillage.remplir.formulaire_entree_page1bis;
-import fr.smardine.matroussedemaquillage.remplir.formulaire_entree_page3;
-import fr.smardine.matroussedemaquillage.variableglobale.ActivityParam;
 import fr.smardine.matroussedemaquillage.variableglobale.EnActionParDefaut;
 import fr.smardine.matroussedemaquillage.variableglobale.ValeurParDefaut;
 
 /**
  * @author smardine
  */
-public class tab1 extends Activity implements OnClickListener,
+public class tab1 extends SuperParamActivity implements OnClickListener,
 		ColorPickerDialog.OnColorChangedListener, OnItemSelectedListener {
 	EditText textView;
 	CheckBox CbAfficheAlerte;
@@ -91,17 +78,7 @@ public class tab1 extends Activity implements OnClickListener,
 		CbAfficheAlerte.setChecked(accesParam.getAfficheAlerte());
 
 		textView.setText("" + accesParam.getDureeViePeremption());
-		// objBd = new BDAcces(this);
-		// // objBd.open();
-		// String[] colonnes = { "AfficheAlerte", "DureeViePeremp", "Theme" };
-		// ArrayList[] Param = objBd.renvoi_param(colonnes);
-		//
-		// if (Param[0].get(0).equals("true")) {
-		// CbAfficheAlerte.setChecked(true);
-		// }
-		// textView.setText(Param[1].get(0).toString());
 
-		// objBd.close();
 		textView.addTextChangedListener(new TextWatcher() {
 
 			// int len=0;
@@ -141,12 +118,6 @@ public class tab1 extends Activity implements OnClickListener,
 
 	}
 
-	@Override
-	protected void onRestart() {
-		super.onRestart();
-		// popUp("onRestart()-Page2");
-	}
-
 	/**
 	 * permet de changer l'icone du widget sur l'objet ImageView
 	 */
@@ -161,58 +132,6 @@ public class tab1 extends Activity implements OnClickListener,
 				break;
 		}
 
-		// objBd = new BDAcces(this);
-		// //objBd.open();
-		// String[] colonnes = { "AfficheAlerte", "DureeViePeremp", "Theme" };
-		// @SuppressWarnings("rawtypes")
-		// ArrayList[] Param = objBd.renvoi_param(colonnes);
-		//
-		// String nomThemeChoisi = Param[2].get(0).toString().trim();
-		// if (EnTheme.Bisounours.getLib().equals(nomThemeChoisi)) {
-		// IvPreviewWidget.setImageResource(R.drawable.icone_bisounours);
-		//
-		// }
-		// if (EnTheme.Fleur.getLib().equals(nomThemeChoisi)) {
-		//
-		// IvPreviewWidget.setImageResource(R.drawable.icone1);
-		// }
-		// objBd.close();
-	}
-
-	/**
-	 * Exécuté lorsque l'activité devient visible à l'utilisateur. La fonction
-	 * onStart() est suivie de la fonction onResume().
-	 */
-	@Override
-	protected void onStart() {
-		super.onStart();
-		// popUp("onStart()-Page2");
-	}
-
-	/**
-	 * Exécutée a chaque passage en premier plan de l'activité. Ou bien, si
-	 * l'activité passe à nouveau en premier (si une autre activité était passé
-	 * en premier plan entre temps). La fonction onResume() est suivie de
-	 * l'exécution de l'activité.
-	 */
-	@Override
-	protected void onResume() {
-		super.onResume();
-		// popUp("onResume()-Page2");
-
-	}
-
-	/**
-	 * La fonction onStop() est exécutée : - lorsque l'activité n'est plus en
-	 * premier plan - ou bien lorsque l'activité va être détruite Cette fonction
-	 * est suivie : - de la fonction onRestart() si l'activité passe à nouveau
-	 * en premier plan - de la fonction onDestroy() lorsque l'activité se
-	 * termine ou bien lorsque le système décide de l'arrêter
-	 */
-	@Override
-	protected void onStop() {
-		super.onStop();
-		// popUp("onStop-Page2");
 	}
 
 	/**
@@ -249,138 +168,6 @@ public class tab1 extends Activity implements OnClickListener,
 			// //objBd.close();
 
 		}
-	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			new majWidget(this, false);
-			Intent intent = null;
-			boolean isLaunchByNotePage1 = getIntent().getBooleanExtra(
-					ActivityParam.LaunchFromNotePage1, false);
-			boolean isLaunchByNoteSaisie = getIntent().getBooleanExtra(
-					ActivityParam.LaunchFromNoteSaisie, false);
-			boolean isLaunchByAfficheDetail = getIntent().getBooleanExtra(
-					ActivityParam.LaunchFromAfficheDetail, false);
-			boolean isLaunchByProduitPerime = getIntent().getBooleanExtra(
-					ActivityParam.LaunchFromRechercheProduitPerime, false);
-			boolean isLaunchByRecherche = getIntent().getBooleanExtra(
-					ActivityParam.LaunchFromRecherche, false);
-			boolean isLaunchByDupplique = getIntent().getBooleanExtra(
-					ActivityParam.LaunchFromDuppliquer, false);
-			boolean isLaunchByPage1 = getIntent().getBooleanExtra(
-					ActivityParam.LaunchFromPage1, false);
-			boolean isLaunchBypagerecap = getIntent().getBooleanExtra(
-					ActivityParam.LaunchFromPageRecap, false);
-			boolean isLaunchByMain = getIntent().getBooleanExtra(
-					"LaunchFromMain", false);
-			if (isLaunchByNotePage1) {
-				intent = new Intent(this, note_page1.class);
-
-			}
-			if (isLaunchByNoteSaisie) {
-				String idNote = getIntent()
-						.getStringExtra(ActivityParam.IdNote);
-				intent = new Intent(this, note_saisie.class);
-				intent.putExtra(ActivityParam.IdNote, idNote);
-			}
-			if (isLaunchByAfficheDetail) {
-				String idProduit = getIntent().getStringExtra(
-						ActivityParam.IdProduit);
-				intent = new Intent(this, affiche_detail.class);
-				intent.putExtra(ActivityParam.IdProduit, idProduit);
-				intent.putExtra(
-						ActivityParam.LaunchFromRecherche,
-						getIntent().getBooleanExtra(
-								ActivityParam.LaunchFromRecherche, false));
-				intent.putExtra(
-						ActivityParam.LaunchFromRechercheProduitPerime,
-						getIntent().getBooleanExtra(
-								ActivityParam.LaunchFromRechercheProduitPerime,
-								false));
-			}
-			if (isLaunchByProduitPerime) {
-				intent = new Intent(this, recherche_produit_perime.class);
-			}
-			if (isLaunchByRecherche) {
-				intent = new Intent(this, Recherche.class);
-			}
-			if (isLaunchByDupplique) {
-				intent = new Intent(this, choix_produit_a_duppliquer.class);
-			}
-			if (isLaunchByPage1) {
-				String MarqueChoisie = getIntent().getStringExtra(
-						ActivityParam.Marque);
-				String DureeVie = getIntent().getStringExtra(
-						ActivityParam.DurreeDeVie);
-				String DateChoisie = getIntent().getStringExtra(
-						ActivityParam.DateAchat);
-				String numTeinte = getIntent().getStringExtra(
-						ActivityParam.NumeroDeTeinte);
-				String nomProduitRecup = getIntent().getStringExtra(
-						ActivityParam.NomProduit);
-
-				intent = new Intent(this, formulaire_entree_page1bis.class);
-				intent.putExtra(ActivityParam.Marque, MarqueChoisie.trim());
-				intent.putExtra(ActivityParam.DurreeDeVie, DureeVie.trim());
-				intent.putExtra(ActivityParam.DateAchat, DateChoisie.trim());
-				intent.putExtra(ActivityParam.NumeroDeTeinte, numTeinte.trim());
-				intent.putExtra(ActivityParam.NomProduit,
-						nomProduitRecup.trim());
-
-			}
-			if (isLaunchBypagerecap) {
-				String MarqueChoisie = getIntent().getStringExtra(
-						ActivityParam.Marque);
-				String DureeVie = getIntent().getStringExtra(
-						ActivityParam.DurreeDeVie);
-				String DateChoisie = getIntent().getStringExtra(
-						ActivityParam.DateAchat);
-				String numTeinte = getIntent().getStringExtra(
-						ActivityParam.NumeroDeTeinte);
-				String nomProduitRecup = getIntent().getStringExtra(
-						ActivityParam.NomProduit);
-
-				intent = new Intent(this, formulaire_entree_page3.class);
-				intent.putExtra(ActivityParam.Marque, MarqueChoisie.trim());
-				intent.putExtra(ActivityParam.DurreeDeVie, DureeVie.trim());
-				intent.putExtra(ActivityParam.DateAchat, DateChoisie.trim());
-				intent.putExtra(ActivityParam.NumeroDeTeinte, numTeinte.trim());
-				intent.putExtra(ActivityParam.NomProduit,
-						nomProduitRecup.trim());
-			}
-			if (isLaunchByMain) {
-				intent = new Intent(this, Main.class);
-			}
-			intent.putExtra(ActivityParam.LaunchFromParam, true);
-			startActivity(intent);
-			termineActivity();
-			return true;
-		}
-		if (keyCode == KeyEvent.KEYCODE_SEARCH) {
-			new majWidget(this, false);
-			Intent intentRecherche = new Intent(this, Recherche.class);
-			// on demarre la nouvelle activité
-			startActivity(intentRecherche);
-			termineActivity();
-		}
-		return super.onKeyDown(keyCode, event);
-	}
-
-	/**
-	 * 
-	 */
-	private void termineActivity() {
-		finish();
-	}
-
-	/**
-	 * 
-	 */
-	public void OnDestroy() {
-		super.onDestroy();
-		// popUp("OnDestroy-Page2");
-
 	}
 
 	@Override
